7e2cdad005c2c22b0ab2ad8b83ab2b8061676489
theunhackable <psrsai001@gmail.com>
2023-04-25 12:34:37 +0530

    no pre warning but completed the task

--- src/components/numbers.jsx
+++ src/components/numbers.jsx
@@ -1,24 +1,116 @@
 import { useState } from "react";
-// numbers
 
-function handleClick(event){
-    console.log(event);
+// calculations
 
+function compute(expression){
+    try{
+        return eval(expression);
+    }
+    catch(exception){
+        return "INVALID EXPRESSION";
+    }
 }
-const Button =(props) => {
-    return <button onClick={ props.handleClick } id={ props.id }>{ props.value }</button>;
-} 
-
-
-
 
 function Calculator(props){
-    const [val, setVal] = useState(0);
-    const [op, setOp] = useState('');
+    const [data, setData] = useState({
+        expression: '0',
+        result: '0',
 
+
+    });
+    const Button =(props) => {
+        return <button onClick={ props.handleClick } id={ props.id } value={ props.value }>{ props.value }</button>;
+    } 
+
+    function handleClick(event){
+        switch(event.target.value){
+            
+            case "0":
+            case "1":
+            case "2":
+            case "3":
+            case "4":
+            case "5":
+            case "6":
+            case "7":
+            case "8":
+            case "9":
+                setData(
+                    (prevData) => {
+                        return {
+                            expression: prevData.expression === "INVALID EXPRESSION" || prevData.expression === "0"? event.target.value: prevData.result + event.target.value,
+                            result: prevData.result === "0"? event.target.value: prevData.result + event.target.value
+                        }
+                    }
+                )
+                break;
+            case "-":
+            case "+":
+            case "*":
+            case "/":
+            case ".":
+                setData(
+                    (prevData) => {
+                        return {
+                            expression: prevData.expression + event.target.value,
+                            result: prevData.result + event.target.value
+                        }
+                    }
+                )
+                break;
+            case "AC":
+                setData(
+                    (prevData) => {
+                        return {
+                            expression: '0',
+                            result: '0'
+                        }
+                    }
+                )
+                break;
+            case "=":
+                const ans = compute(data.expression);
+                if(ans !== "INVALID EXPRESSION") {
+                    const rounded = Math.round(ans*10000)/10000;
+                    setData(
+                        (prevData) => {
+                            return {
+                                expression: prevData.expression + "=" + rounded,
+                                result: rounded
+                            }
+                        }
+                    )
+                }
+                else {
+                    setData(
+                        (prevData) => {
+                            return {
+                                expression: ans,
+                                result: '0'
+                            }
+                        }
+                    )
+                }
+
+                
+                break;
+
+            default: 
+                break;
+        }
+        console.log("expression=",data.expression);
+        console.log("result=",data.result);
+
+    }
+// 3 + 5 * 6 - 2 / 4
     return (
         <div id="container">
-            <div id="display">888888888888888</div>
+            
+            <div id="display">
+                {/* <p id="expression">{ data.expression }</p>
+                <p id="result"> { data.result } </p> */}
+                { data.result }
+            </div>
 
             <Button id="clear" handleClick={ handleClick } value="AC" />
             <Button id="divide" handleClick= { handleClick } value="/"/>
